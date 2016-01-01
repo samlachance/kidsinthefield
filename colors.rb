06f06cb6383b438f3649c07ac10ee17f3333d53e
Sam Lachance <slachance@gmail.com>
2016-01-01 12:48:11 -0500

    Completed basic styling, added equations, added jquery menu for conditions

--- colors.rb
+++ colors.rb
@@ -1,24 +1,24 @@
 def weight_color(kg)
   case kg
     when 0..5
-      { color: "grey", hex_color: "47d645" }
+      { color: "gray", hex_color: "9E9E9E", age: "newborn", hr: "100", rr: "30", bp: "90" }
     when 5..7
-      { color: "pink", hex_color: "47d645" }
+      { color: "pink", hex_color: "E91E63", age: "4 months", hr: "100", rr: "30", bp: "90" }
     when 6..9
-      { color: "red", hex_color: "fb4f4f" }
+      { color: "red", hex_color: "fb4f4f", age: "8 months", hr: "100", rr: "30", bp: "90" }
     when 9..11
-      { color: "purple", hex_color: "47d645" }
+      { color: "purple", hex_color: "673AB7", age: "1 year", hr: "100", rr: "30", bp: "90" }
     when 11..14
-      { color: "yellow", hex_color: "fbc93d" }
+      { color: "yellow", hex_color: "fbc93d", age: "2 years", hr: "100", rr: "30", bp: "90" }
     when 14..18
-      { color: "white", hex_color: "47d645" }
+      { color: "white", hex_color: "F8F8F8", age: "4 years", hr: "100", rr: "30", bp: "90" }
     when 18..23
-      { color: "blue", hex_color: "6cc0e5" }
+      { color: "blue", hex_color: "6cc0e5", age: "6 years", hr: "100", rr: "30", bp: "90" }
     when 23..29
-      { color: "orange", hex_color: "47d645" }
+      { color: "orange", hex_color: "FF5722", age: "8 years", hr: "100", rr: "30", bp: "90" }
     when 29..36
-      { color: "green", hex_color: "47d645" }
+      { color: "green", hex_color: "4CAF50", age: "10 years", hr: "100", rr: "30", bp: "90" }
     else
-      { color: "adult", hex_color: "ffffff"}
+      { color: "adult", hex_color: "ffffff", age: ">12 years", hr: "100", rr: "30", bp: "90" }
   end
 end
--- drug_calculations.rb
+++ drug_calculations.rb
@@ -1,5 +1,5 @@
 def tylenol(kg)
-  kg * 15
+  (kg * 15).round
 end
 
 def tylenol_volume(kg)
@@ -11,16 +11,51 @@ def fluid_bolus(kg)
 end
 
 def morphine(kg)
-  kg * 0.1
+  (kg * 0.1).round(1)
 end
 
 def ativan(kg)
+  dose = (kg * 0.1).round(3)
+  if dose > 4
+    "4"
+  else
+    dose
+  end
 end
 
 def zofran(kg)
   if kg < 12
-    2
+    { dose: 2, equation: "< 12kg"}
   else
-    4
+    { dose: 4, equation: "> 12kg"}
   end
 end
+
+def epi10(kg)
+  (kg * 0.01).round(3)
+end
+
+def epi1(kg)
+end
+
+def atropine_first(kg)
+end
+
+def atropine_second(kg)
+end
+
+def adenosine(kg)
+end
+
+def amiodarone(kg)
+end
+
+def defib_first(kg)
+end
+
+def defib_second(kg)
+end
+
+def cardiovert(kg)
+end
+
